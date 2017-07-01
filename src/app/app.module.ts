894789310656ff90dd2f998e6ce87b9511f3562a
Hendrik Lammers <lammers@gmail.com>
2017-07-01 15:05:51 +0200

    Add editor module & component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from './shared/shared.module';
+import { EditorModule } from './editor/editor.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    EditorModule,
     SharedModule
   ],
   providers: [],
